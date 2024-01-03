eae01256391e0a120f4d9c849ef585ced0db4576
juan bernal <mesieou@gmail.com>
2024-01-03 15:03:30 +1100

    added js to activate boxes like checkboxes

--- 02-My-Favourite-Sports/lib/index.js
+++ 02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,13 @@
 // TODO: write your code here!
+let sportBoxesElement = document.querySelectorAll('.clickable');
+const sportBoxesArray = Array.from(sportBoxesElement)
+
+const activateBox = (event) => {
+  event.currentTarget.classList.toggle('active')
+};
+
+const bindBoxToClick = (box) => {
+  box.addEventListener('click', activateBox);
+};
+
+sportBoxesArray.forEach(bindBoxToClick);
