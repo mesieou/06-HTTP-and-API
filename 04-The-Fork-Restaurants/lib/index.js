715576e94a40d2ba56dd8eee90ff94f79f4b314a
juan bernal <mesieou@gmail.com>
2024-01-04 14:39:44 +1100

    added the event listener with the api call

--- 04-The-Fork-Restaurants/lib/index.js
+++ 04-The-Fork-Restaurants/lib/index.js
@@ -1,4 +1,8 @@
 // Todo: Use the Fork Api to get a filtered list of restaurants, depending on the category selected (and as a bonus, the location)
+const constructEndPoint = (category) => {
+  return `https://the-fork-api.students.lewagon.co/api/v1/restaurants?category=${category}`
+};
+
 
 const searchForm = document.getElementById("searchForm");
 
@@ -7,12 +11,20 @@ const searchForm = document.getElementById("searchForm");
 
 searchForm.addEventListener("submit", (event) => {
   // Todo: Find the category selected and build the URL you will send your request to
+  event.preventDefault();
+  const allcuisineElements = document.querySelectorAll('.form-check-input')
+  const allcuisineElementsArray = Array.from(allcuisineElements)
+  const cuisineSelectedElement = allcuisineElementsArray.filter(element => element.checked);
 
+  const categorySelected = cuisineSelectedElement[0].defaultValue
+  const endPoint = constructEndPoint(categorySelected)
+
+  const restaurantsHtmlContainer = document.querySelector('.list-group')
   // Todo: Replace "the-endpoint-url" with the URL you built
-  fetch("the-endpoint-url")
+  fetch(endPoint)
     .then(response => response.json())
     .then((data) => {
-      // Todo: Insert results into the list
-
+      const restaurantsHtml = data.map(restaurant => `<li class="list-group-item">${restaurant.name}</li>`).join('')
+      restaurantsHtmlContainer.innerHTML = restaurantsHtml;
     });
 })
