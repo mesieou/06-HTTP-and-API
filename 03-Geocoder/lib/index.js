59990d1c460675316169d70bba9233b2c740dfe4
juan bernal <mesieou@gmail.com>
2024-01-04 12:58:31 +1100

    added the showMap function

--- 03-Geocoder/lib/index.js
+++ 03-Geocoder/lib/index.js
@@ -1,9 +1,9 @@
 const geoCodeButton = document.querySelector('form');
 let userInput = document.querySelector('form input');
-
+const apiKey = 'pk.eyJ1IjoianVhbmRhOTMiLCJhIjoiY2xxaHQ2eThqMDFlNTJudGRxemR2eXd6bSJ9.kGsejmgnLUR7B208_qNcpg'
+let coordinatesArray = []
 
 const constructUrl = (userInput) => {
-  const apiKey = 'pk.eyJ1IjoianVhbmRhOTMiLCJhIjoiY2xxaHQ2eThqMDFlNTJudGRxemR2eXd6bSJ9.kGsejmgnLUR7B208_qNcpg'
   return `https://api.mapbox.com/geocoding/v5/mapbox.places/${userInput}.json?access_token=${apiKey}`;
 };
 
@@ -11,9 +11,11 @@ const formatAndDisplayCoordinates = (data) => {
   const coordinatesElement = document.querySelector('.font-monospace');
   const longitude = data.features[0].center[0];
   const latitude = data.features[0].center[1];
+  coordinatesArray.push(longitude, latitude)
   const coordinates = `${longitude}, ${latitude}`;
 
   coordinatesElement.textContent = coordinates;
+  showMap(apiKey)
 };
 
 const displayCoordinates = (userInput) => {
@@ -22,17 +24,19 @@ const displayCoordinates = (userInput) => {
     .then( data => formatAndDisplayCoordinates(data));
 }
 
+
+const showMap = (apiKey) => {
+  mapboxgl.accessToken = apiKey
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: coordinatesArray,
+    zoom: 12
+  });
+};
+
 geoCodeButton.addEventListener('submit', (event) => {
   event.preventDefault();
   userInput = userInput.value;
   displayCoordinates(userInput);
 });
-
-const showMap = (userInput) => {
-  // console.log(userInput)
-  // displayCoordinates(coordinates)
-  // - Display the coordinates in the element where the coordinates will be displayed
-      // - Create a map using the Mapbox API and the coordinates
-      // - Add a marker to the map at the coordinates
-
-};
