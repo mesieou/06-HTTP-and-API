cb8e8e5a702b436f42e16af50a8611d3b493e6e7
juan bernal <mesieou@gmail.com>
2024-01-04 15:56:02 +1100

    added location search to the api

--- 04-The-Fork-Restaurants/lib/index.js
+++ 04-The-Fork-Restaurants/lib/index.js
@@ -1,8 +1,8 @@
 // Todo: Use the Fork Api to get a filtered list of restaurants, depending on the category selected (and as a bonus, the location)
 const searchForm = document.getElementById("searchForm");
 
-const constructEndPoint = (category) => {
-  return `https://the-fork-api.students.lewagon.co/api/v1/restaurants?category=${category}`
+const constructEndPoint = (cuisine, location) => {
+  return `https://the-fork-api.students.lewagon.co/api/v1/restaurants?category=${cuisine}&location=${location}`
 };
 
 const instertHtmlData = (data) => {
@@ -16,12 +16,18 @@ const getCuisineSelected = () => {
   const allcuisineElementsArray = Array.from(allcuisineElements)
   const cuisineSelectedElement = allcuisineElementsArray.filter(element => element.checked);
   return cuisineSelectedElement[0].defaultValue
-}
+};
+
+const getLocation = () => {
+  return document.querySelector('#location').value
+};
 
 searchForm.addEventListener("submit", (event) => {
-  const endPoint = constructEndPoint(getCuisineSelected())
   event.preventDefault();
+  const endPoint = constructEndPoint(getCuisineSelected(), getLocation())
   fetch(endPoint)
     .then(response => response.json())
-    .then((data) => instertHtmlData(data));
+    .then((data) => {
+      console.log(data)
+      instertHtmlData(data)});
 })
