8da3b3f0bcaa7d51dc388286ef947459e09e7d11
juan bernal <mesieou@gmail.com>
2024-01-04 17:22:33 +1100

    finished the basic api call

--- 05-Weather/lib/index.js
+++ 05-Weather/lib/index.js
@@ -1,18 +1,29 @@
-// TODO: Call the Weather API when the form is submitted
+const form = document.querySelector('form')
 
-// TODO: Create a function to get the weather info
-const fetchWeather = () => {
-  // TODO: Replace the following line with the correct url
-  // TODO: prevent default behavior of the form
-  const url = '...'
-
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      // TODO: Insert the weather info in the DOM (description, date, temperature...)
-    });
+const constructUrl = () => {
+  const city = document.querySelector('#input').value
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3610274c79ad18f14591bc2e9ae44b31&units=metric`
 }
 
-// TODO: Add an event listener to the form
-// TODO: On submit, in the callback, call the getWeatherInfo function
+const insertHTML = (data) => {
+  const cityHtml = document.querySelector('#city')
+  const descriptionHtml = document.querySelector('#description')
+  const temperatureHtml = document.querySelector('#temperature')
+
+  cityHtml.innerHTML = data.name
+  descriptionHtml.innerHTML = data.weather[0].description
+  temperatureHtml.innerHTML = data.main.temp
+}
+
+const fetchWeather = () => {
+  const url = constructUrl()
+
+  fetch(url)
+  .then(response => response.json())
+  .then((data) => insertHTML(data));
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  fetchWeather();
+});
