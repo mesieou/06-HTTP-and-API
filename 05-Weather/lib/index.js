1d8814c98a5f159c0741fa3c14435d306637cf77
juan bernal <mesieou@gmail.com>
2024-01-04 17:59:44 +1100

    added current date

--- 05-Weather/lib/index.js
+++ 05-Weather/lib/index.js
@@ -1,5 +1,18 @@
 const form = document.querySelector('form')
 
+const formattedDate = () => {
+  const currentDate = new Date();
+  const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(currentDate);
+}
+
 const constructUrl = () => {
   const city = document.querySelector('#input').value
   return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3610274c79ad18f14591bc2e9ae44b31&units=metric`
@@ -9,15 +22,19 @@ const insertHTML = (data) => {
   const cityHtml = document.querySelector('#city')
   const descriptionHtml = document.querySelector('#description')
   const temperatureHtml = document.querySelector('#temperature')
+  const iconHtml = document.querySelector('#icon')
+  const dateHtml = document.querySelector('#date')
+  const url = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
 
-  cityHtml.innerHTML = data.name
-  descriptionHtml.innerHTML = data.weather[0].description
-  temperatureHtml.innerHTML = data.main.temp
+  cityHtml.innerHTML = data.name;
+  descriptionHtml.innerHTML = data.weather[0].description;
+  temperatureHtml.innerHTML = data.main.temp;
+  iconHtml.src = url;
+  dateHtml.textContent = formattedDate();
 }
 
 const fetchWeather = () => {
   const url = constructUrl()
-
   fetch(url)
   .then(response => response.json())
   .then((data) => insertHTML(data));
