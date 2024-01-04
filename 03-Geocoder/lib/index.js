1a0ea3a0b45b3ca9e9c06f0df1c8404d8f16ace8
juan bernal <mesieou@gmail.com>
2024-01-04 13:09:58 +1100

    fix bug to search multiple places

--- 03-Geocoder/lib/index.js
+++ 03-Geocoder/lib/index.js
@@ -1,21 +1,29 @@
 const geoCodeButton = document.querySelector('form');
-let userInput = document.querySelector('form input');
+let userInputElement = document.querySelector('form input');
 const apiKey = 'pk.eyJ1IjoianVhbmRhOTMiLCJhIjoiY2xxaHQ2eThqMDFlNTJudGRxemR2eXd6bSJ9.kGsejmgnLUR7B208_qNcpg'
-let coordinatesArray = []
 
-const constructUrl = (userInput) => {
-  return `https://api.mapbox.com/geocoding/v5/mapbox.places/${userInput}.json?access_token=${apiKey}`;
+const constructUrl = (userInputElement) => {
+  return `https://api.mapbox.com/geocoding/v5/mapbox.places/${userInputElement}.json?access_token=${apiKey}`;
+};
+
+const showMap = (longitude, latitude) => {
+  mapboxgl.accessToken = apiKey
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: [longitude, latitude],
+    zoom: 12
+  });
 };
 
 const formatAndDisplayCoordinates = (data) => {
   const coordinatesElement = document.querySelector('.font-monospace');
   const longitude = data.features[0].center[0];
   const latitude = data.features[0].center[1];
-  coordinatesArray.push(longitude, latitude)
   const coordinates = `${longitude}, ${latitude}`;
 
   coordinatesElement.textContent = coordinates;
-  showMap(apiKey)
+  showMap(longitude, latitude)
 };
 
 const displayCoordinates = (userInput) => {
@@ -24,19 +32,8 @@ const displayCoordinates = (userInput) => {
     .then( data => formatAndDisplayCoordinates(data));
 }
 
-
-const showMap = (apiKey) => {
-  mapboxgl.accessToken = apiKey
-  const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v9",
-    center: coordinatesArray,
-    zoom: 12
-  });
-};
-
 geoCodeButton.addEventListener('submit', (event) => {
   event.preventDefault();
-  userInput = userInput.value;
+  const userInput = userInputElement.value;
   displayCoordinates(userInput);
 });
